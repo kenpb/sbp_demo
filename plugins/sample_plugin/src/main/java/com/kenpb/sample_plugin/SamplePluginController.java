f0ed5136eec3ff88ec20d814614f020793b7c6cd
kenpb <kpalmab@icloud.com>
2024-03-14 05:58:16 +0000

    feat: added htmx-spring-boot

--- plugins/sample_plugin/src/main/java/com/kenpb/sample_plugin/SamplePluginController.java
+++ plugins/sample_plugin/src/main/java/com/kenpb/sample_plugin/SamplePluginController.java
@@ -1,16 +1,18 @@
 package com.kenpb.sample_plugin;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 
 @Controller
 @RequestMapping("/sample-plugin")
 public class SamplePluginController {
 
+    @HxRequest
     @GetMapping
-    public String index(Model model) {
+    public String htmxRequest() {
         return "plugin";
     }
 
